9903d8827d698a3cf2fc2c973e49f5908c4f0561
somdari <matthewchoi7@gmail.com>
2017-01-14 22:44:20 +1100

    Initializing and setting state in SearchBar component

--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -1,6 +1,13 @@
 import React from 'react';
 
 class SearchBar extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      searchTerm: '',
+    };
+    this.handleInputChange = this.handleInputChange.bind(this);
+  }
   handleInputChange(event) {
     console.log(event.target.value);
   }
