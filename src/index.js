c8145122adb1e66192a506ecc449ba8c766c08ab
somdari <matthewchoi7@gmail.com>
2017-01-14 11:06:44 +1100

    Linting setup

--- src/index.js
+++ src/index.js
@@ -1,9 +1,2 @@
-import React from "react"
-import ReactDom from "react-dom"
-import Greeting from './greeting';
-
-ReactDom.render(
-    <Greeting name="Jack"/>,
-    document.getElementById('container')
-
-);
+const foo = "bar";
+foo.toUpperCase();
