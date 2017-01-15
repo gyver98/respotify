109f0f1e8e51b5b2bd214c891d6825efdbc1e9ef
somdari <matthewchoi7@gmail.com>
2017-01-15 18:22:55 +1100

    Retrieve tracks with TrackList, Track component

---
+++ src/components/Track.js
@@ -0,0 +1,29 @@
+import React from 'react';
+const mouseOutColor = 'white';
+const mouseOverColor = '#ADD8E6';
+
+const Track = (props) => {
+  return (
+    <li
+      style={Track.styles.li}
+      onMouseOver={(e) => { e.target.style.backgroundColor = mouseOverColor; }}
+      onMouseOut={(e) => { e.target.style.backgroundColor = mouseOutColor; }}
+    >
+      {props.track.name}
+    </li>
+  );
+};  
+Track.propTypes = {
+  track: React.PropTypes.object.isRequired,
+};
+
+Track.styles = {
+  li: {
+    fontSize: '1.5em',
+    padding: '0.2em',
+    listStyleType: 'none',
+    backgroundColor: mouseOutColor,
+  },
+};
+
+export default Track;
