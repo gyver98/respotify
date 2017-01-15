91da60ef78ed4b2ec3d89815c4a46999e33e6e41
somdari <matthewchoi7@gmail.com>
2017-01-15 11:31:55 +1100

    AlbumList component

---
+++ src/components/AlbumList.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+const AlbumList = (props) => {
+  const albums = props.albums.map((album) => <li>{album.name]</li>);
+  
+  return (
+    <ul>
+      {albums}
+    </ul>
+  );
+};
+
+AlbumList.propTypes = {
+  albums: React.PropTypes.array.isRequired,
+};
+
+export default AlbumList;
--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import SearchBar from './components/SearchBar';
 
+
 class App extends React.Component {
   render() {
     return (
